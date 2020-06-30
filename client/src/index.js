445988bee5919272f5bd83a389e9633803de4ab6
Akash Paul <akash@akashpaul.com>
2020-06-30 22:24:04 +0530

    added worker path

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './js/App';
 import './css/app.scss';
-
+import * as serviceWorker from './js/serviceWorker';
 render(<App />, document.getElementById('root'));
+serviceWorker.register();
--- server/index.js
+++ server/index.js
@@ -6,8 +6,11 @@ const socket = require('./lib/socket');
 const app = express();
 const server = http.createServer(app);
 
-app.use('/', express.static(`${__dirname}/../client`));
 
+app.get('/worker.js', (req, res) => {
+  res.sendFile('./assets/worker.js', { root: __dirname });
+});
+app.use('/', express.static(`${__dirname}/../client`));
 server.listen(config.PORT, () => {
   socket(server);
   console.log('Server is listening at :', config.PORT);
